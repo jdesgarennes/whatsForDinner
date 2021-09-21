c4c9e3d41de5e0ce3130031a026707d33006652e
Kelly R Cano <krcano2001@gmail.com>
2021-09-20 17:54:09 -0800

    updated api fetch  request

--- assets/script.js
+++ assets/script.js
@@ -3,6 +3,8 @@
 var ingredientInput = document.querySelector(".ingredientInput")
 var ingredientsList = document.querySelector(".ingredientsList")
 var searchIngredient= document.querySelector('#searchIngredient');
+var fetchButton = document.getElementById("searchIngredient");
+
 
 
 
@@ -15,19 +17,32 @@ function getIngredient() {
 
     var requestUrl = 'https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/food/products/classify?locale=en_us'+ ingredientInput.value;
 
+    fetch("https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/findByIngredients?ingredients=apples%2Cflour%2Csugar&number=5&ignorePantry=true&ranking=1", {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
+		"x-rapidapi-key": "9e46aff44dmshfaaaf6ad37243c9p1b4948jsn65e37fc6e624"
+	}
+})  
+.then(response => {
+	console.log(response);
+})
+.catch(err => {
+	console.error(err);
+});
   
-    fetch(requestUrl)
-      .then(function (response) {
-        return response.json();
-      })
-      .then(function (data) {
-        for (var i = 0; i < data.length; i++) {
-          var listItem = document.createElement('li');
-          listItem.textContent = data[i].ingredientInput.value;
-          ingredientsList.appendChild(listItem);
-        }
-        console.log(getIngredient)
-      });
-  }
-  
+    // fetch(requestUrl)
+    //   .then(function (response) {
+    //     return response.json();
+    //   })
+//       .then(function (data) {
+//         for (var i = 0; i < data.length; i++) {
+//           var listItem = document.createElement('li');
+//           listItem.textContent = data[i];
+//           ingredientsList.appendChild(listItem);
+//         }
+//         console.log(response)
+//       });
+//   }
+}
   fetchButton.addEventListener('click', getIngredient);
