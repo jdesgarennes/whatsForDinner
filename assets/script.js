fac363c208bbc63582d38184b528285a9348d284
Kelly R Cano <krcano2001@gmail.com>
2021-09-22 19:27:54 -0800

    added tostring and replaceAll fetch works

--- assets/script.js
+++ assets/script.js
@@ -5,7 +5,8 @@ var ingredientsList = document.querySelector(".ingredientsList")
 var searchIngredient= document.querySelector('#searchIngredient');
 var submitButton = document.getElementById("submitIngredient");
 var listContainer = document.querySelector(".itemListContainer")
-var searchRecipeButt = document.querySelector(".searchRecipes")
+var searchRecipeButt = document.querySelector("#searchRecipes")
+var itemListString=""
 var itemList =[]
 
 
@@ -18,17 +19,20 @@ var itemList =[]
 function getIngredient() {
 
     var requestUrl = 'https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/food/products/classify?locale=en_us'+ ingredientInput.value;
-
-    fetch("https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/findByIngredients?ingredients=" + ingredientInput.value + "&number=5&ignorePantry=true&ranking=1", {
+    console.log(itemListString)
+    fetch("https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/findByIngredients?ingredients=" + itemListString + "&number=5&ignorePantry=true&ranking=1", {
 	"method": "GET",
 	"headers": {
 		"x-rapidapi-host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
 		"x-rapidapi-key": "9e46aff44dmshfaaaf6ad37243c9p1b4948jsn65e37fc6e624"
-	}
-})  
+        
+	} 
+   
+}) 
+
 .then(response => {
    return response.json();
-})
+}) 
 .then (data =>{
     console.log(data)
 })
@@ -44,10 +48,8 @@ function recipeList(){
         var li = document.createElement("li");
         li.textContent = itemLists;
         listContainer.appendChild(li);
-    }
-}
+    }}
 
-getIngredient()
 
 // takes the ingredient list value and pushes it to the item list array so that its usable outsided of the function too
 function ingredientPush(array){
@@ -55,6 +57,7 @@ function ingredientPush(array){
     ingredientText.innerHTML ="";
     array.push(ingredientText);
     console.log(itemList);
+    
 }
 
 
@@ -64,10 +67,13 @@ function ingredientPush(array){
     ingredientPush(itemList);
     recipeList();
   });
-  console.log(itemList);
+ 
 
-   searchRecipeButt.addEventListener('click', function(){
-      
+searchRecipeButt.addEventListener('click', function(){
+     itemListString = itemList.toString().replaceAll(",", "&");
+     getIngredient()
+
+    console.log(itemListString)
    })
 
 
